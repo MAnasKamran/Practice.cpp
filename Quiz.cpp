3a7f72a78921d790513fb8d8433ce9561d9863eb
M.Anas Kamran <studanns1@gmail.com>
2026-07-25 20:17:47 +0500

    Refactor quiz program for improved readability

--- Quiz.cpp
+++ Quiz.cpp
@@ -9,7 +9,7 @@ int main(){
 
 
     std::string options[][4] = {{"A. 1969", "B. 1975", "C. 1985", "D. 2000"},
-                                {"A. Guido van Rossum", "B. Bjarne Stroustrup", "C. John Carmack", "Mark Zuckerburg"},
+                                {"A. Guido van Rossum", "B. Bjarne Stroustrup", "C. John Carmack", "D. Mark Zuckerburg"},
                                 {"A. C", "B. C+", "C. C--", "D. B++"},
                                 {"A. override", "B. virtual", "C. static", "D. polymorph"}};
 
