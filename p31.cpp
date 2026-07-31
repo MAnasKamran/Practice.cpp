69eca4c459f73d70af22e8beb50a7b9c4ba6ad61
Anas <studanns1@gmail.com>
2026-07-31 22:08:13 +0500

    OOP: Constructors in C++.

---
+++ p31.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class Student{
+    public:
+    std::string name;
+    int age;
+    double gpa;
+
+    Student(std::string name, int age, double gpa){
+        this->name = name;
+        this->age = age;
+        this->gpa = gpa;
+    }
+    void printDetails(){
+        std::cout << "Name: " << name << '\n';
+        std::cout << "Age: " << age << '\n';
+        std::cout << "GPA: " << gpa << "\n\n";
+    }
+};
+int main(){
+    Student student1("John Doe", 22,3.77);
+    student1.printDetails();
+
+    Student student2("Alice", 21, 3.68);
+    student2.printDetails();
+
+    Student student3("Oliver", 23, 3.84);
+    student3.printDetails();
+    
+    return 0;
+}
