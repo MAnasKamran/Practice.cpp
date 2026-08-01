15bbdd30511f9b48d1c97db441d06c7f2d585781
Anas <studanns1@gmail.com>
2026-08-01 08:30:14 +0500

    Inheritance

--- p34.cpp
+++ p34.cpp
@@ -15,9 +15,20 @@ class Dog : public Animal{
         std::cout << "The dog is eating.\n";
     }
 };
+
+class Cat : public Animal{
+    public: 
+    void eat(){
+        std::cout << "The cat is eating.\n";
+    }
+};
+
 int main(){
     Dog dog;
     std::cout << (dog.alive ? "The dog is alive.\n" : "The dog is dead.\n");
     dog.eat(); 
+    Cat cat;
+    std::cout << (cat.alive ? "The cat is alive.\n" : "The cat is dead.\n");
+    cat.eat();
     return 0;
 }
