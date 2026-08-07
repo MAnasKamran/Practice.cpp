aca460982546549319440395bd583b4a296f64c4
Anas <studanns1@gmail.com>
2026-08-07 18:10:05 +0500

    Bit operators

---
+++ bitOpPract.cpp
@@ -0,0 +1,21 @@
+# include <iostream>
+
+using namespace std;
+int main(){
+
+    // AND operator
+    cout << (6 & 10) << '\n';
+
+    //OR operator
+    cout << (6 | 10) << '\n';
+    
+    //XOR operator
+    cout << (6 ^ 10) << '\n';
+
+    //Left shift << operator
+    cout << (10 << 2) << '\n';
+
+    //Right shift >> operator
+    cout << (10 >> 1) << '\n';
+    return 0;
+}
