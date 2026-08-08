3b2856532c1075116daab157a32162f646353e43
Anas <studanns1@gmail.com>
2026-08-08 14:59:57 +0500

    Check if the number is a power of two.

---
+++ powOfTwoCheck.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+//using loop:
+
+int checkPowOfTwo(int n){
+    if(n <= 1){
+        return false;
+    }
+    while(n % 2 == 0){
+        n /= 2;
+    }
+    return n == 1;
+}
+
+//using bit-wise operator:
+bool checkPOfTwo(int n){
+    return n > 0 && (n & (n - 1)) == 0;
+}
+int main(){
+    int num;
+    cout << "Enter the number: ";
+    cin >> num;
+    cout << "The number " << num << " is " << (checkPowOfTwo(num) ? "" : "not") << " a power of two.\n";
+
+    cout << "The number " << num << " is " << (checkPOfTwo(num) ? "" : "not") << " a power of two.";
+
+    return 0;
+}
